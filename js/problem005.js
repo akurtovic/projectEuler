15d342163505edc5e21e17af0dbe9c281519b8de
Amir Kurtovic <amirkurtovic@gmail.com>
2015-06-21 14:57:12 -0500

    Adding js solution for problem 5

---
+++ js/problem005.js
@@ -0,0 +1,17 @@
+var answerFound = false;
+var n = 20;
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+while(!answerFound) {
+  n++;
+  var matchedNumbers = 0;
+  numbers.forEach(function(number) {
+    if (n%number === 0) {
+      matchedNumbers++;
+    }
+  })
+  if (matchedNumbers === 20) {
+    answerFound = true;
+    console.log("n=",n);
+  }
+}
